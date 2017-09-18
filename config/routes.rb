b5dc40790e5ca742b7e9e28044e215fb6585b7a3
Greg <gregmarczak@outlook.com>
2017-09-18 22:20:38 +0000

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
-  root to: "posts#index"
+  get 'posts/index'
+
+  devise_for :users
+  devise_for :installs
+  root 'places#index'
+    resources :places do
+      resources :comments, only: :create
+    end
 end
 
-
-
-
-
 #Rails.application.routes.draw do
-#  get 'posts/index'
-#
-#  devise_for :users
-#  devise_for :installs
-#  root 'places#index'
-#    resources :places do
-#      resources :comments, only: :create
-#    end
+#  root to: "posts#index"
 #end
