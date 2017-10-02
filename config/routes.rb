094e25c784be34f82c87bb1a161c9ecce9910d42
Greg <gregmarczak@outlook.com>
2017-10-02 17:41:05 +0000

    13-47 user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :places do
       resources :comments, only: :create
     end
+    resources :users, only: :show
 end
 
 Rails.application.routes.draw do
